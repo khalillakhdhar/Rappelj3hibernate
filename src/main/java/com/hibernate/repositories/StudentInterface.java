8e902a67de071a44ef597cb37cefb006763571cf
khalil <khalillakhdharatc@gmail.com>
2022-03-16 13:46:35 +0100

    Update StudentInterface.java

--- src/main/java/com/hibernate/repositories/StudentInterface.java
+++ src/main/java/com/hibernate/repositories/StudentInterface.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hibernate.model.manytomany.Student;
 
 public interface StudentInterface extends JpaRepository<Student, Long> {
-List<Student> findByName(String name);
+/*List<Student> findByName(String name);
 List<Student> findByGrade(String grade);
 List<Student> findByGradeAndName(String grade,String name);
+*/
 //Optional<Student> findById(long id); // retourne un seule étudiant
 // Optional => soit une instance soit null
 }
